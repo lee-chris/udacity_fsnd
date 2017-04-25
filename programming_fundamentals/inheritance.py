563234797fe38cb891c8d7d299a2c513ca05a456
Chris Lee <lee.christopher.e@gmail.com>
2017-04-24 23:24:17 -0700

    added simple method overriding example

--- programming_fundamentals/inheritance.py
+++ programming_fundamentals/inheritance.py
@@ -12,6 +12,12 @@ class Parent():
         
         self.last_name = last_name
         self.eye_color = eye_color
+    
+    
+    def show_info(self):
+        
+        print("Last name = " + self.last_name)
+        print("Eye color = " + self.eye_color)
 
 
 # inherits from Parent
@@ -24,11 +30,21 @@ class Child(Parent):
         Parent.__init__(self, last_name, eye_color)
         
         self.number_of_toys = number_of_toys
+        
+        
+    # override method from Parent
+    def show_info(self):
+        
+        print("Last name = " + self.last_name)
+        print("Eye color = " + self.eye_color)
+        print("Number of toys = " + str(self.number_of_toys))
 
 
 billy_cyrus = Parent("Cyrus", "blue")
-print(billy_cyrus.last_name)
+#print(billy_cyrus.last_name)
+billy_cyrus.show_info()
 
 miley_cyrus = Child("Cyrus", "blue", 5)
-print(miley_cyrus.last_name)
-print(miley_cyrus.number_of_toys)
+#print(miley_cyrus.last_name)
+#print(miley_cyrus.number_of_toys)
+miley_cyrus.show_info()
