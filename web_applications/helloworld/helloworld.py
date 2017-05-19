26e8500e44b3251919f14dc30fb1fcaba464d12e
Chris Lee <lee.christopher.e@gmail.com>
2017-05-18 23:26:04 -0700

    added simple url handler to perform a google search

--- web_applications/helloworld/helloworld.py
+++ web_applications/helloworld/helloworld.py
@@ -1,12 +1,29 @@
 ﻿import webapp2
 
+form = """
+<form action="http://www.google.com/search">
+    <input name="q">
+    <input type="submit">
+</form>
+"""
+
 class MainPage(webapp2.RequestHandler):
     
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.headers["Content-Type"] = "text/plain"
+        self.response.write("Hello, World!")
         
+
+class Google(webapp2.RequestHandler):
+    
+    def get(self):
+        self.response.headers["Content-Type"] = "text/html"
+        self.response.write(form)
+        
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ("/", MainPage),
+    ("/search", Google)
 ], debug=True)
     
