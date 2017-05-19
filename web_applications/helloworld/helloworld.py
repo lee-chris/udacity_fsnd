543b397a2b6e49e65bd563b80486a8d4bf458e35
Chris Lee <lee.christopher.e@gmail.com>
2017-05-18 23:33:45 -0700

    added simple form that submits to a different request handler

--- web_applications/helloworld/helloworld.py
+++ web_applications/helloworld/helloworld.py
@@ -1,13 +1,7 @@
 ﻿import webapp2
 
-form = """
-<form action="http://www.google.com/search">
-    <input name="q">
-    <input type="submit">
-</form>
-"""
-
 class MainPage(webapp2.RequestHandler):
+    """Simple hello world"""
     
     def get(self):
         self.response.headers["Content-Type"] = "text/plain"
@@ -15,15 +9,43 @@ class MainPage(webapp2.RequestHandler):
         
 
 class Google(webapp2.RequestHandler):
+    """Form used to do a Google search"""
+    
+    form = """
+    <form action="http://www.google.com/search">
+        <input name="q">
+        <input type="submit">
+    </form>
+    """
     
     def get(self):
         self.response.headers["Content-Type"] = "text/html"
-        self.response.write(form)
-        
+        self.response.write(self.form)
+
+
+class SimpleForm(webapp2.RequestHandler):
+    """Simple form that submits to TestHandler"""
+    
+    def get(self):
+        self.response.write("""
+            <form action="/testForm">
+                <input name="q">
+                <input type="submit">
+            </form>
+        """)
+
+
+class TestHandler(webapp2.RequestHandler):
+    
+    def get(self):
+        q = self.request.get("q")
+        self.response.out.write(q)
 
 
 app = webapp2.WSGIApplication([
     ("/", MainPage),
-    ("/search", Google)
+    ("/search", Google),
+    ("/input", SimpleForm),
+    ("/testForm", TestHandler)
 ], debug=True)
     
