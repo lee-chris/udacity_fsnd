19176f0193e349d73ed38eafdc6da06797205011
Chris Lee <lee.christopher.e@gmail.com>
2017-05-28 22:52:48 -0700

    added FormHandler
    This example generates an html form and then reads the values in the
    form submission

--- web_applications/helloserver/helloserver.py
+++ web_applications/helloserver/helloserver.py
@@ -1,3 +1,5 @@
+import urllib.parse
+
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class HelloHandler(BaseHTTPRequestHandler):
@@ -31,7 +33,50 @@ class EchoHandler(BaseHTTPRequestHandler):
         self.wfile.write(message_bytes)
 
 
+class FormHandler(BaseHTTPRequestHandler):
+    """Example of GET/POST using a simple form"""
+    
+    def do_GET(self):
+        
+        self.send_response(200)
+        
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        
+        html = """
+            <html>
+                <body>
+                    <form method="post">
+                        <div>Login: <input name="l"/></div>
+                        <div>Password: <input type="password" name="p"/></div>
+                        <div><input type="submit" value="Submit"/></div>
+                    </form>
+                </body>
+            </html>
+            """
+        
+        self.wfile.write(html.encode())
+    
+    def do_POST(self):
+        
+        length = int(self.headers.get("Content-length", 0))
+        data = self.rfile.read(length).decode()
+        query_string = urllib.parse.parse_qs(data)
+        
+        login = query_string["l"][0]
+        password = query_string["p"][0]
+        
+        message = "login: " + login + "\npassword: " + password
+        
+        self.send_response(200)
+        
+        self.send_header("Content-type", "text/plain; charset=utf-8")
+        self.end_headers()
+        
+        self.wfile.write(message.encode())
+
+
 if __name__ == "__main__":
     server_address = ("", 8000) #Serve on all addresses, port 8000
-    httpd = HTTPServer(server_address, EchoHandler)
+    httpd = HTTPServer(server_address, FormHandler)
     httpd.serve_forever()
