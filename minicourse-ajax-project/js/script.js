e3a363a4075a68b30d0165182158f7691d251f73
Chris Lee <lee.christopher.e@gmail.com>
2017-12-22 14:47:22 -0800

    added NY Times article search results.

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -14,7 +14,22 @@ function loadData() {
     // load streetview
     $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + $('#street').val() + ' ' + $('#city').val() + '">');
 
-    // YOUR CODE GOES HERE!
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'api-key': NYT_API_KEY,
+        'q': $('#city').val()
+    });
+    $.getJSON(url, function(result) {
+        
+        console.log(result);
+        
+        var docs = result.response.docs;
+        
+        for (var i = 0; i < docs.length; i++) {
+            $('#nytimes-articles').append('<li class="article"><a href="' + docs[i].web_url + '">' + docs[i].headline.main + '</a><p>' + docs[i].snippet + '</p></li>');
+        }
+    
+    });
 
     return false;
 };
