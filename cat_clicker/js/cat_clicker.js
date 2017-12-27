138c170b3b395ce6ae41fd2e52f681f5112c7bb4
Chris Lee <lee.christopher.e@gmail.com>
2017-12-27 14:07:01 -0800

    updated cat clicker to display 2 cats.

--- cat_clicker/js/cat_clicker.js
+++ cat_clicker/js/cat_clicker.js
@@ -1,10 +1,40 @@
-var click_count = 0;
+function cat(props) {
+    
+    var that = {};
+    
+    that.name = props.name;
+    that.img = "img/cat.jpeg";
+    that.click_count = 0;
+    
+    return that;
+}
 
-function cat_click() {
-    click_count++;
-    $("#count").html(click_count);
+function get_cat_html(cat) {
+    
+    var container_div = $('<div class="cat-container"></div>');
+    
+    var count_div = $('<div class="count">0</div>');
+    $(count_div)
+        .appendTo(container_div);
+    
+    var name_div = $('<div class="name"></div>');
+    $(name_div)
+        .html(cat.name)
+        .appendTo(container_div);
+    
+    $('<img class="cat"/>')
+        .attr("src", cat.img)
+        .click(function() {
+            cat.click_count++;
+            $(count_div).html(cat.click_count);
+        })
+        .appendTo(container_div);
+    
+    return container_div;
 }
 
 $(function() {
-    $("#cat").click(cat_click);
+    $("#main")
+        .append(get_cat_html(cat({name : 'Simon'})))
+        .append(get_cat_html(cat({name : 'Ben'})));
 });
